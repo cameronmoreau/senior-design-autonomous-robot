e96fc6777e0294d00cd588cbc2fa47a104995d8e
Cameron Moreau <cameron.cm6@gmail.com>
2018-03-21 14:40:22 -0500

    name changes

--- pi/main.py
+++ pi/main.py
@@ -4,14 +4,15 @@ from game_manager import *
 from gui import *
 from robot_controller import *
 from localization_manager import *
+from navigation_manager import *
 
 if __name__ == '__main__':
   game = GameManager('config.json')
   robot = RobotController(simulate=True)
-  local = LocalizationManager(game.path)
+  nav = NavigationManager(game.path)
 
   print('finding path between 1 and 15')
-  p, c = local.get_route('1', '15')
+  p, c = nav.get_route('1', '15')
   print('path', str(p), 'cost', c)
 
   root = tk.Tk()
