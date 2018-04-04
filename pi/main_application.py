cef815214374ff00550458aa90d9094d797f6705
IEEE Robot <robot@uta.edu>
2018-04-04 15:55:39 +0000

    temp

--- pi/main_application.py
+++ pi/main_application.py
@@ -24,32 +24,34 @@ class MainApplication():
     self.nav = NavigationManager(self.game.path)
     self.local = LocalizationManager(robot=self.robot, game=self.game, start_x=380, start_y=0)
     #self.vision = VisionManager(vertex_callback=self.local.on_vertex_change, direction_callback=self.local.on_direction_change)
-    self.vision = VisionManager(vertex_callback=self.local.on_vertex_change, direction_callback=self.direction_callback)
+    self.vision = VisionManager(vertex_callback=self.vertex_callback, direction_callback=self.direction_callback)
 
     # UI
     root = tk.Tk()
     self.gui = GuiApplication(master=root, vision=self.vision, game_manager=self.game, robot=self.robot, localization_manager=self.local)
      
     # TEMP
-    self.robot.move_raw(200, 200, -200, -200)
+    self.robot.move_raw(-300, -300, -300, -300)
     #self.local.subscribe_to_events(self.new_waypoint)
     #self.last_waypoint = None
-    
+
+  def vertex_callback(self, vert):
+    print('at a vertex!!', vert)   
+ 
   def direction_callback(self, direction):
     can_update = (time.time() - self.last_time) > 0.4
     if direction < -0.3 and can_update:
       print("GO RIGHT last time")
       self.last_time = time.time()
-      self.robot.move_raw(50, 200, -50, -200)
+      self.robot.move_raw(-300, -100, -300, -100)
     elif direction > 0.3 and can_update:
       print("GO LEFT")
       self.last_time = time.time()
-      self.robot.move_raw(200, 50, -200, -50)
+      self.robot.move_raw(-100, -300, -100, -300)
     elif direction > -0.3 and direction < 0.3 and can_update:
-       self.robot.move_raw(200, 200, -200, -200)
+       self.robot.move_raw(-300, -300, -300, -300)
        self.last_time = time.time()
        print('STRAINGT')
-    print(str(direction))
     
   def new_waypoint(self):
     # On new waypoint
